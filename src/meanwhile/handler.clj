fb9010d7387757a5e8c6bbc7803058aff0bef6dd
Leena Wooller <leena.wooller@yle.fi>
2015-05-28 18:01:48 +0300

    Decade from parameter.

--- src/meanwhile/handler.clj
+++ src/meanwhile/handler.clj
@@ -4,10 +4,17 @@
             [clj-http.client :as client]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+(defn- parse-int [s]
+  (Integer. (re-find  #"\d+" s )))
+
+(defn- get-decade [decade-start]
+  (str decade-start "/" (+ (parse-int decade-start) 9)))
+
 (defroutes app-routes
   (GET "/" [] "Hello World")
-  (GET "/archive" [] (client/get " http://haku.yle.fi/api/search" {:query-params {:category "elavaarkisto" :UILanguage "fi"
-                                                                       :decade "1970/1979" :media "video" :page "1"}}))
+  (GET "/archive/:decade" [decade] (client/get "http://haku.yle.fi/api/search" {:query-params {:category "elavaarkisto" :UILanguage "fi"
+                                                                       :decade (get-decade decade) :media "video" :page "1"}}))
+  (GET "/video" [] (client/get "http://yle.fi/aihe/artikkeli/2015/04/23/holmbergin-jaakarin-morsian-kohahdutti-maltillisuudellaan"))
   (route/not-found "Not Found"))
 
 (def app
